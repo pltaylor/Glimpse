84b00503b4c522b7747eee1d5bf4883cdb37135a
pltaylor <pltaylor3@gmail.com>
2013-12-19 15:20:54 -0500

    First Attempt at Load Tests

---
+++ Glimpse.Test.MVCLoadTests/MVCLoadTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using Xunit;
+
+namespace Glimpse.Test.MVCLoadTests
+{
+    public class MVCLoadTest:SeleniumTest
+    {
+        public MVCLoadTest() : base("source\\Glimpse.Mvc4.MusicStore.Sample") { }
+
+        
+        [Fact]
+        public void LoadTest()
+        {
+            //Arrange
+            int totalCount = 5;
+            // Act
+
+            Parallel.For(0, totalCount, i =>
+            {
+                var driver = new FirefoxDriver();
+                driver.Url = GetAbsoluteUrl("/");
+
+                for (int j = 0; j < 100; j++)
+                {
+                    driver.Url = GetAbsoluteUrl("/Store/Browse?Genre=Jazz");
+                    driver.Url = GetAbsoluteUrl("/Store/Browse?Genre=Electronic");
+                    driver.Url = GetAbsoluteUrl("/Store/Browse?Genre=Alternative");
+                    driver.Url = GetAbsoluteUrl("/Store/Browse?Genre=Classical");
+                    driver.Url = GetAbsoluteUrl("/Store/Browse?Genre=Rap");
+                    driver.Url = GetAbsoluteUrl("/Store/Browse?Genre=Latin");
+                    driver.Url = GetAbsoluteUrl("/Store/Browse?Genre=Blues");
+                    driver.Url = GetAbsoluteUrl("/Store/Browse?Genre=Metal");
+                    driver.Url = GetAbsoluteUrl("/Store/Browse?Genre=Rock");
+                    driver.Url = GetAbsoluteUrl("/Store");
+                    driver.Url = GetAbsoluteUrl("/Account/Login?ReturnUrl=%2fStoreManager");
+                }
+
+
+                driver.Quit();
+            });
+
+            //Assert
+
+            Assert.True(true);
+        }
+    }
+}
---
+++ Glimpse.Test.MVCLoadTests/SeleniumTest .cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Glimpse.Test.MVCLoadTests
+{
+    public class SeleniumTest : IDisposable
+    {
+        
+        const int iisPort = 2020;
+        private string _applicationName;
+        private Process _iisProcess;
+ 
+        protected SeleniumTest(string applicationName) 
+        {
+            _applicationName = applicationName;
+            // Start IISExpress
+            StartIIS();
+        }
+ 
+        public void Dispose() 
+        {
+            // Ensure IISExpress is stopped
+            if (_iisProcess.HasExited == false) {
+                _iisProcess.Kill();
+            }
+        }
+ 
+        private void StartIIS() {
+            var applicationPath = GetApplicationPath(_applicationName);
+            var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+
+            //C:\Users\ptaylor.CORP\Documents\GitHub\Glimpse\source\Glimpse.Mvc4.MusicStore.Sample
+
+            _iisProcess = new Process();
+            _iisProcess.StartInfo.FileName = programFiles + "\\IIS Express\\iisexpress.exe";
+            _iisProcess.StartInfo.Arguments = string.Format("/path:\"{0}\" /port:{1}", applicationPath, iisPort);
+            _iisProcess.Start();
+        }
+ 
+ 
+        protected virtual string GetApplicationPath(string applicationName) {
+            var solutionFolder = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)));
+            return Path.Combine(solutionFolder, applicationName);
+        }
+ 
+ 
+        public string GetAbsoluteUrl(string relativeUrl) {
+            if (!relativeUrl.StartsWith("/")) {
+                relativeUrl = "/" + relativeUrl;
+            }
+            return String.Format("http://localhost:{0}{1}", iisPort, relativeUrl);
+        }
+ 
+ 
+    }
+}
